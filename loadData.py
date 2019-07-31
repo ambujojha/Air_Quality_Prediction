d893eccc37f478c273ccd512960d81a0d8174523
Ambuj Ojha <ambujojha@Ambujs-MacBook-Pro.local>
2019-07-30 22:26:01 -0400

    Added dataloader

--- loadData.py
+++ loadData.py
@@ -3,13 +3,13 @@ import requests
 from requests.exceptions import HTTPError
 import json
 
-for url in [#'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160101&edate=20160229&state=06'
-            #,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160301&edate=20160430&state=06'
-            #,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160501&edate=20160630&state=06'
-            #,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160701&edate=20160831&state=06'
-            #,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160901&edate=20161031&state=06'
-            #,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20161101&edate=20161231&state=06'
-            'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20170101&edate=20170228&state=06'
+for url in ['https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160101&edate=20160229&state=06'
+            ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160301&edate=20160430&state=06'
+            ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160501&edate=20160630&state=06'
+            ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160701&edate=20160831&state=06'
+            ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20160901&edate=20161031&state=06'
+            ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20161101&edate=20161231&state=06'
+            ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20170101&edate=20170228&state=06'
             ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20170301&edate=20170430&state=06'
             ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20170501&edate=20170630&state=06'
             ,'https://aqs.epa.gov/data/api/sampleData/byState?email=test@aqs.api&key=test&param=88101&bdate=20170701&edate=20170831&state=06'
